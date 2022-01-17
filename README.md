a595259e31af9842ddee3c0fc02a21722a8c4f9e
Zackery Arsement <zarsement@hotmail.com>
2022-01-17 14:19:00 -0600

    Application is complete. Started completing the README file.

--- README.md
+++ README.md
@@ -1,1 +1,26 @@
 # weatherDashboard_HW6
+Title: City Weather Forecast \
+Developer: Zackery Arsement \
+Deployment Date:  1/17/2022 \
+For:  UT JavaScript Coding Bootcamp \
+Published: GitHub <https://github.com/ZackeryArsement/weatherDashboard_HW6> \
+Published: Webpage <https://zackeryarsement.github.io/weatherDashboard_HW6/> \
+Built with: HTML, CSS, Javascript, Bootstrap, Jquery
+
+
+# Summary: 
+
+![Coding Quiz](https://zackeryarsement.github.io/dailyPlanner_HW5/assets/images/dailyPlanner.png)
+
+The daily planner allows you to input a 'to-do list' for any given day! \
+You can save your input by clicking the corresponding button on the right side of each hour. \
+If you refresh the page, your list will still be there! \
+The planner keeps track of what time it is to let you know if you have already passed your to-do time.
+
+![Coding Quiz](https://zackeryarsement.github.io/dailyPlanner_HW5/assets/images/dailyPlanner.gif)
+
+# Development:
+
+This webpage utilizes both the 'moment()' method and the windows local storage. The webpage starts up by creating an array of time values from 9AM to 5PM. There is then a 'for loop' ran that creates nine input segments, each corresponding to a time value. A repeating function is then put into place to check every minute if the time has passed a new hour. If the time has passed a new hour then the code changes the color of the inputs in order to match the desired color pattern: gray means the hour has already passed, red means you are within that specific hour, and green means that that hour has not yet came.
+
+When the user presses the save button on the right side of the input segment then the code will save the input value into local storage and display the word 'Saved' onto the button. On start up, when the input segments are being created, the code will put the locally saved input value into the corresponding input segment. The button will also keep its displayed 'Saved' text. If you attempt to save an input segment that contains no text then nothing happens. Furthermore, if you refresh the page and you have a saved segment that does not contain text anymore then that button will lose its 'save' status.
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,24 +1,194 @@
 var city = 'Austin';
 var apiKey = '9ffb53b585866877578daabea92018de';
-var apiURL = 'https://api.openweathermap.org/data/2.5/onecall?lat=30.2672&lon=-97.7431&exclude=hourly,minutely&appid=' + apiKey;
+var apiURL = 'https://api.openweathermap.org/data/2.5/onecall?lat=30.2672&lon=-97.7431&exclude=hourly,minutely&units=imperial&appid=' + apiKey;
+
+var today = moment();
 
 var forecastCardArray = $('.forecast-card');
-// console.log(forecastCardArray[0].children['card-temp'].innerText);
+var forecastTitle = $('.results-list');
 
-var dailyTemp = [];
+var searchButton = $('#search-btn');
+var inputCity = $('#input-city');
 
-getCityWeather();
+// List of buttons with recent cities searched
+var recentSearchList = $('#recent-search-list');
 
+// Keep track of recent cities searched
+var savedSearches = JSON.parse(localStorage.getItem("savedSearches")) || ["","","","",""];
+
+// localStorage.clear();
+
+// On webpage loadup access the current weather and 5 day forecast for Austin, Tx
+getData("https://wft-geo-db.p.rapidapi.com/v1/geo/cities?limit=1&offset=0&minPopulation=100000&namePrefix=" + city);
+loadRecents();
+
+// When you press the search button you append a new button to the recently searched column and then find the weather for that city
+function searchCity(){
+    city = inputCity.val();
+
+    var findCityURL = "https://wft-geo-db.p.rapidapi.com/v1/geo/cities?limit=10&offset=0&minPopulation=90000&namePrefix=" + city;
+
+    getData(findCityURL);
+}
+
+// Get the weather for the longitude and latitude of your searched city
 function getCityWeather(){
     fetch(apiURL)
     .then(function (response){
         return response.json();
     })
     .then(function (data){
-        console.log(data);
+
+        // Current Temp
+        forecastTitle[0].children['day-temp'].children[0].innerText = 'Temperature: ' + data.daily[0].temp.day + ' \u00B0F '
+
+        // Current Wind
+        forecastTitle[0].children['day-wind'].children[0].innerText = 'Wind: ' + data.daily[0].wind_speed + ' mph';
+        
+        // Current Humidity
+        forecastTitle[0].children['day-humidity'].children[0].innerText = 'Humidity: ' + data.daily[0].humidity + '%';
+
+        // Current UV Index
+        forecastTitle[0].children['day-uv'].children[0].innerText = 'UV Index: ' + data.daily[0].uvi;
+
+        // Fill in the 5 day forecast
         for(let i=0; i<5; i++){
-            forecastCardArray[i].children['card-temp'].innerText = data.daily[(i+1)].temp.day;
+            var forecastDay = moment(today).add((i+1), 'days').format('MM/DD/YYYY');
+
+            // Date
+            forecastCardArray[i].children['card-date'].children[0].innerText = forecastDay;
+
+            // Icon
+            forecastCardArray[i].children['card-icon'].children[0].src = ' http://openweathermap.org/img/wn/' + data.daily[(i+1)].weather[0].icon + '.png';
+
+            // Temp
+            forecastCardArray[i].children['card-temp'].innerText = data.daily[(i+1)].temp.day + ' \u00B0F ';
+
+            // Wind
+            forecastCardArray[i].children['card-wind'].innerText = data.daily[(i+1)].wind_speed + ' mph';
+
+            // Humidity
+            forecastCardArray[i].children['card-humidity'].innerText = data.daily[(i+1)].humidity + '%';
         }
-        return dailyTemp;
+
+        appendSearchButton();
     })
+}
+
+// Get the longitude and latitude for your searched city so you can use that in your weather search input
+function getData(URL){
+    fetch(URL, {
+        "method": "GET",
+        "headers": {
+            "x-rapidapi-host": "wft-geo-db.p.rapidapi.com",
+            "x-rapidapi-key":"dc52e1306fmsh499b745225e0ed7p1f9230jsnd35ba4c60dc2" //this is the key for the paid subscription
+        }
+        })
+        .then(response => {
+            return response.json();
+        })
+        .then(function(data){
+            var userLat = data.data[0].latitude;
+            var userLng = data.data[0].longitude;
+
+            var region = data.data[0].region;
+
+            forecastTitle[0].children['day-city'].children[0].innerText = city + ', ' + region + ' (' + today.format('MM/DD/YYYY') + ')';
+
+            apiURL = 'https://api.openweathermap.org/data/2.5/onecall?lat=' + userLat + '&lon='+ userLng + '&exclude=hourly,minutely&units=imperial&appid=' + apiKey;
+
+            // Get the weather for the longitude and latitude of your searched city
+            getCityWeather();
+
+            return data;
+        })
+        .catch(err => {
+            alert('The city you searched is not in our files. Please search a different city.');
+            console.error(err);
+        });
+}
+
+// If you click a 'recently searched city' list button
+function searchRecent(btn){
+    city = btn.innerText;
+
+    var findCityURL = "https://wft-geo-db.p.rapidapi.com/v1/geo/cities?limit=1&offset=0&minPopulation=100000&namePrefix=" + city;
+
+    getData(findCityURL);
+}
+
+// On browser start up load in all the recently searched cities into buttons
+function loadRecents(){
+    for(let i=0; i<savedSearches.length; i++){
+        if(savedSearches[i] !== ''){
+            var newButton = document.createElement('button');
+            newButton.classList.add('btn', 'btn-secondary', 'btn-lg', 'btn-block', 'm-2');
+
+            // Every new button has an onclick that searched the city it is attached to
+            newButton.onclick = function(event){
+                var currentButton = event.target;
+                searchRecent(currentButton);
+            }
+
+            newButton.innerText = savedSearches[i];
+            
+            recentSearchList.append(newButton);
+        }
+    }
+}
+
+function appendSearchButton(){
+    var savedSearchCount = 0;
+
+    // See how many cities are saved in recent searches
+    savedSearches.forEach(search => {
+        if(search !== ''){
+            savedSearchCount++;
+        }
+    })
+
+    // If the searched city is not already in the 'recently searched' list then create a button for the input city and save it in local storage
+    if(!savedSearches.includes(city)){
+        if(savedSearchCount === 5){
+            // Remove the oldest searched city button and the city name from saved list
+            savedSearches.splice(savedSearches.indexOf(recentSearchList[0].children[0].innerText), 1, '');
+            recentSearchList[0].children[0].remove();
+
+            var newButton = document.createElement('button');
+            newButton.classList.add('btn', 'btn-secondary', 'btn-lg', 'btn-block', 'm-2');
+            
+            // Every new button has an onclick that searched the city it is attached to
+            newButton.onclick = function(event){
+                var currentButton = event.target;
+                searchRecent(currentButton);
+            }
+    
+            newButton.innerText = city;
+            
+            recentSearchList.append(newButton);
+    
+            // Creates new array with values meant to be saved
+            savedSearches.splice(savedSearches.indexOf(''), 1, city);
+            localStorage.setItem('savedSearches', JSON.stringify(savedSearches));
+
+        }
+        else{
+            var newButton = document.createElement('button');
+            newButton.classList.add('btn', 'btn-secondary', 'btn-lg', 'btn-block', 'm-2');
+            
+            // Every new button has an onclick that searched the city it is attached to
+            newButton.onclick = function(event){
+                var currentButton = event.target;
+                searchRecent(currentButton);
+            }
+    
+            newButton.innerText = city;
+            
+            recentSearchList.append(newButton);
+    
+            // Creates new array with values meant to be saved
+            savedSearches.splice(savedSearches.indexOf(''), 1, city);
+            localStorage.setItem('savedSearches', JSON.stringify(savedSearches));
+        }
+    }
 }
